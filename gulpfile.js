21aad312c335776ddcf04ce56615cef2ad2c1cc6
git <Levi.Currit@tivityhealth.com>
2018-08-24 16:35:52 -0700

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var del = require('del');
+var runSequence = require('run-sequence');
+var shell = require('gulp-shell');
+
+gulp.task('sass', function () {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'expanded'
+		}))
+		.pipe(cleanCSS())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}));
+});
+
+gulp.task('images', function () {
+	return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+		.pipe(cache(imagemin()))
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', function () {
+	return gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('useref', function () {
+	return gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpIf('*.js', uglify()))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean:dist', function () {
+	return del.sync('dist');
+});
+
+gulp.task('browserSync', function () {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		}
+	});
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/js/**/*.js', browserSync.reload);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('build', function (callback) {
+	runSequence('clean:dist', 'sass', ['images', 'fonts'], 'useref', callback);
+});
+
+gulp.task('default', function (callback) {
+	runSequence(['watch'], callback);
+});
+
+gulp.task('push', function (callback) {
+	runSequence(['build'], shell.task('scp dist/* tilde.town:~/public_html/', callback));
+});
