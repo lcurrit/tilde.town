b6aa1f202ce3301574de31cb906a7ae0f62143b6
git <Levi.Currit@tivityhealth.com>
2018-09-25 16:17:40 -0700

    Updated styling and animations

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
 console.log('scripts loaded');
+
+$('a').click(function(e) {
+	e.preventDefault();
+	if($(this).data('details')) {
+		var id = $(this).data('details');
+		$('.sections > *').fadeOut('slow', function() {
+			$('#' + id).fadeIn('slow');
+		});
+	}
+});
