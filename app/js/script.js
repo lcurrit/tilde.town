b8e92716a4d133c0f05d57c6f1e6ffd2a1a8b30a
Levi <levi.currit@gmail.com>
2018-10-08 19:18:14 -0700

    Changes

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,7 @@
-console.log('scripts loaded');
+// Console Shenanigans
+console.log("%c%s","color: rgb(33, 37, 41); background: #ffbfbf; font-size: 20px; padding:10px;", "Do you want to see my dog?");
+console.log("%c%s","color: rgb(33, 37, 41); background: rgb(204, 255, 128); font-size: 20px; padding:10px;", "Of course you do:");
+console.log("%c%s","color: rgb(33, 37, 41); background: aqua; font-size: 20px; padding:10px;", window.location.href + "img/luna.jpg");
 
 // Hide / Show Sections
 $('.menu a').click(function(e) {
@@ -6,7 +9,7 @@ $('.menu a').click(function(e) {
 	if($(this).data('details')) {
 		var id = $(this).data('details');
 		$('.sections > *').fadeOut('slow').promise().done(function() {
-			$('#' + id).fadeIn('slow');
+			$('#' + id).css("display", "flex").hide().fadeIn('slow');
 		});
 	}
 });
