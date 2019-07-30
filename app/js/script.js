8cf5f845d694416d0c3c5142ac6bf4649947caf4
Levi Currit <levi.currit@gmail.com>
2019-07-29 20:31:44 -0700

    Audited & Height Fix

--- app/js/script.js
+++ app/js/script.js
@@ -3,13 +3,23 @@ console.log("%c%s","color: rgb(33, 37, 41); background: #ffbfbf; font-size: 20px
 console.log("%c%s","color: rgb(33, 37, 41); background: rgb(204, 255, 128); font-size: 20px; padding:10px;", "Of course you do:");
 console.log("%c%s","color: rgb(33, 37, 41); background: aqua; font-size: 20px; padding:10px;", window.location.href + "img/luna.jpg");
 
+// Setting Variable to Support Full Min-Height Flex
+// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+document.addEventListener("DOMContentLoaded", () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 $(function(){
 
 	// Hide / Show Sections
 	$('.menu a').click(function(e) {
 		e.preventDefault();
 		var $this = $(this);
-		var $parentLI = $this.parent('li');
 		if($this.data('details')) {
 			var id = $this.data('details');
 			$('.sections > *').fadeOut('slow').promise().done(function() {
